a6730552c1eae09a77b4f4bb4f2675b7970214d1
Valeria-CPaz <val.paz@gmail.com>
2025-09-28 16:02:43 -0300

    fix: ajuste de CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://localhost:3000",
+        "https://case-autou-gray.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
